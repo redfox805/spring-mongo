ebbcd347fd76b391217ed904cc797483305a6532
hisyamzayd <hisyamzayd@gmail.com>
2022-02-02 16:38:01 +0700

    init commit

---
+++ src/main/java/com/zaydhisyam/samsungtest/SamsungTestApplication.java
@@ -0,0 +1,13 @@
+package com.zaydhisyam.samsungtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsungTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsungTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaydhisyam/samsungtest/SamsungTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaydhisyam.samsungtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsungTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
